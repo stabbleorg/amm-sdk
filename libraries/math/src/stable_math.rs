61658c34c879519f93f9b788e0cb9aeb61c4e2f9
angadsingh <angad17@hotmail.com>
2025-12-04 23:43:53 +0530

    Increase MAX_AMP to 12_000

--- libraries/math/src/stable_math.rs
+++ libraries/math/src/stable_math.rs
@@ -7,7 +7,7 @@ use bn::{
 pub const AMP_PRECISION: u64 = 1_000;
 
 pub const MIN_AMP: u16 = 1;
-pub const MAX_AMP: u16 = 8000;
+pub const MAX_AMP: u16 = 12000;
 
 pub const MIN_SWAP_FEE: u64 = 10_000; // 0.001%
 pub const MAX_SWAP_FEE: u64 = 10_000_000; // 1%
--- packages/sdk/src/accounts/stable-pool.ts
+++ packages/sdk/src/accounts/stable-pool.ts
@@ -21,7 +21,7 @@ export class StablePool implements Pool<StablePoolData> {
   static POOL_TOKEN_SIZE = 32 + 1 + 1 + 8 + 8;
 
   static MIN_AMP = 1;
-  static MAX_AMP = 8000;
+  static MAX_AMP = 12000;
 
   static MIN_SWAP_FEE = 0.00001; // 0.001%
   static MAX_SWAP_FEE = 0.01; // 1%
